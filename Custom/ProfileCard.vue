d26d639617d61b9986e1eda40c20a7295f659a9d
Radames Ajna <radamajna@gmail.com>
2020-01-30 17:29:11 -0800

    person profile card

---
+++ Custom/ProfileCard.vue
@@ -0,0 +1,120 @@
+C<template>
+  <div class="container">
+    <h3>
+      {{ profileData.figure }}
+    </h3>
+    <div>
+      <img
+        :src="profileData.picture"
+        class="card-img"
+      >
+    </div>
+    <div
+      :class="['box-collapse', isCollapsed? 'collapsed': '']"
+      @click="toggleBox"
+    >
+      <div class="ico-align">
+        <span class="icon-arrow-bold-down ico-effect" />
+      </div>
+      <ul>
+        <template v-for="item in profileData.list">
+          <li :key="item">
+            {{ item }}
+          </li>
+        </template>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    profileData: {
+      type: Object,
+      require: true,
+      default: null
+    }
+  },
+  data () {
+    return {
+      isCollapsed: true
+    }
+  },
+  computed: {
+  },
+  methods: {
+    toggleBox () {
+      this.isCollapsed = !this.isCollapsed
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/css/mixins";
+@import "~@/css/vars";
+
+.container {
+  position: relative;
+}
+.card-img {
+  box-shadow: 10px -10px 0px $black;
+  shape-outside: url(/profiles/profile.jpg);
+  width: 100%;
+  object-fit: cover;
+  position: absolute;
+  left: -1rem;
+  top: -1rem;
+  z-index: 10;
+  max-width: 90px;
+
+  @include breakpoint(medium) {
+    max-width: 150px;
+  }
+}
+.box-collapse {
+  cursor: pointer;
+  border: 2px solid rgba($black, 0.8);
+  transition: all 0.25s ease-in-out;
+  max-height: 1000px;
+  overflow: hidden;
+}
+.collapsed {
+  max-height: 5rem;
+  .ico-effect {
+    transform: unset;
+  }
+}
+ul {
+  position: relative;
+  list-style: outside disc;
+  padding: 0;
+  margin: 0;
+  margin-bottom: 1rem;
+  li {
+    position: relative;
+    left: 2rem;
+    padding-right: 1.5rem;
+  }
+}
+h3 {
+  margin-left: auto;
+  text-align: right;
+  padding-bottom: 0;
+  max-width: 50%;
+  @include breakpoint(medium) {
+    max-width: unset;
+  }
+}
+.ico-align {
+  text-align: right;
+  margin-top: 1rem;
+  margin-right: 1rem;
+}
+.ico-effect {
+  display: inline-block;
+  transition: all 0.25s ease-in-out;
+  transform: rotate(-180deg);
+}
+</style>
