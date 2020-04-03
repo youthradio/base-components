07c46da19637ddf3562fecc89a8fb26093d97adf
shagia <usenet@shagia.me>
2020-04-03 12:30:18 -0700

    add PollComponent and make props for component

---
+++ Custom/PollComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="quiz-question">
+      <h3> {{ questionSet.question }} </h3>
+    </div>
+    <div class="quiz-answers">
+      <button>{{ questionSet.A }}</button>
+      <button>{{ questionSet.B }}</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    questionSet: {
+      type: Object,
+      required: true,
+      default: null
+    }
+  },
+  mounted () {
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import "~@/css/mixins";
+@import "~@/css/vars";
+
+</style>
