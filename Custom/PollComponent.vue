29eb7ec5ddfd1df605c81c390e202bd07fc2e492
Radames Ajna <radamajna@gmail.com>
2020-04-13 23:35:56 -0700

    poll fetch backend

--- Custom/PollComponent.vue
+++ Custom/PollComponent.vue
@@ -19,7 +19,10 @@
       v-else
       class="quiz-result"
     >
-      You selected {{ selectedButton }}!
+      <div class="content">
+        <strong> {{ result.total }}% </strong>
+        choose {{ result.title }}
+      </div>
     </div>
   </div>
 </template>
@@ -34,16 +37,28 @@ export default {
   },
   data () {
     return {
-      selectedButton: null,
-      hasVoted: false
+      hasVoted: false,
+      result: null
     }
   },
   mounted () {
   },
   methods: {
-    submitVote (vote) {
-      // maybe an async function has to sit between the end of this function and changing the data to calculate a percentage of people who've voted for what? maybe an api'll handle that for us?
-      this.selectedButton = vote
+    async submitVote (vote) {
+      const res = await fetch(
+        `https://ee51aej7u4.execute-api.us-west-2.amazonaws.com/latest/vote_poll/${this.questionSet.id}/${vote.id}`,
+        {
+          method: 'GET',
+          credentials: 'include'
+        }
+      ).then(res => res.json())
+      const total = res.poll.options.reduce((a, i) => a + i.count, 0) // adds total counts
+      const selectedVote = res.poll.options.find(i => i.id === vote.id) // find count for voted option
+      const percentage = 100 * selectedVote.count / total
+      this.result = {
+        total: percentage.toFixed(),
+        title: vote.title
+      }
       this.hasVoted = true
     }
   }
@@ -71,24 +86,41 @@ h3 {
   place-content: center;
   flex-basis: 70%;
   background-color: $darkblue;
-  color: white;
+  color: $white;
   border-radius: 0.5rem;
   padding: 1rem;
   margin: 0.5rem;
+}
+.quiz-result {
+  flex-basis: 30%;
+  display: flex;
+  flex-grow: 1;
+  place-items: center;
+  background-color: $lightgrey;
+  border-radius: 0.5rem;
+  padding: 1rem;
+  margin: 0.5rem;
+
+  .content {
+    margin: auto;
+    font-size: larger;
+  }
 }
 
 .quiz-answers {
   display: flex;
   justify-content: center;
+  flex-basis: 30%;
   @include breakpoint(medium) {
     flex-direction: column;
   }
   //First button
   button {
+    cursor: pointer;
     flex-basis: 50%;
     text-align: center;
     background: unset;
-    border: 0.5rem solid $darkblue;
+    border: 0.2rem solid $darkblue;
     border-radius: 0.5rem;
     font-size: 1rem;
     font-weight: 300;
