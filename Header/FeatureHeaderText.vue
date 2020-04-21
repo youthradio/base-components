767998939fbcc8a2231bec880fa1b2c9c02ab9ca
Radames Ajna <radamajna@gmail.com>
2020-04-21 15:30:29 -0700

    new headline format

--- Header/FeatureHeaderText.vue
+++ Header/FeatureHeaderText.vue
@@ -15,8 +15,8 @@
         type="video/ogv"
       >
       <source
-        :src="`./${headerData.featureImage}.webm`"
-        type="video/webm"
+        :src="`./${headerData.featureImage}.webp`"
+        type="video/webp"
       >
       <source
         :src="`./${headerData.featureImage}.mp4`"
@@ -32,10 +32,11 @@
       class="img-fluid lazyload"
       @load="updateHeaderHeight"
     > -->
-    <div class="base-header title">
-      <h1 v-html="headerData.title" />
-      <h3> {{ headerData.subtitle }}</h3>
-
+    <div class="base-header">
+      <div class="title">
+        <h1 v-html="headerData.title" />
+        <h3 v-html="headerData.subtitle" />
+      </div>
       <!-- <h3> by {{ headerData.author }}</h3>
       <h5> {{ headerData.publishDate }}</h5> -->
     </div>
@@ -78,13 +79,14 @@ export default {
   width: 100%;
   height: auto;
   object-fit: cover;
+  max-height: 100vh;
 }
 .feature-container {
   position: relative;
   display: flex;
   flex-direction: column;
   background-color: $darkblue;
-  padding-bottom: 56.25%;
+  padding-bottom: MIN(56.25%, 100vh);
   height: 0;
   background: radial-gradient(circle at 25% 10%, #292e49 20%, transparent 100%),
     radial-gradient(circle at 70% 30%, #6b5b67 50%, transparent 100%),
@@ -94,49 +96,54 @@ export default {
 .base-header {
   position: absolute;
   display: flex;
-  place-items: center;
-  place-content: center;
   flex-direction: column;
   width: 100%;
   height: 100%;
 }
 .title {
+  width: 100%;
+  max-width: 35rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  margin: auto auto 2rem auto;
   h1,
   h2,
   h3 {
-    font-family: "Assistant";
-    font-style: normal;
-    font-weight: 500;
     color: $white;
     text-shadow: 0px 0px 20px black;
-  }
-  h3 {
-    font-size: 1.2rem;
-    @include breakpoint(medium) {
-      font-size: 1.5rem;
-    }
-    &:first-of-type {
-      // margin-left: -30%;
-    }
-    &:last-of-type {
-      // margin-right: -30%;
-    }
-  }
-  h1 {
-    padding: 1rem;
-    text-transform: uppercase;
-    font-size: 5rem;
-    line-height: 3.5rem;
-    @include breakpoint(medium) {
-      font-size: 6rem;
-      line-height: 4.5rem;
-    }
-    font-weight: 800;
+
     /deep/ span {
       &::before {
         content: "\A";
         white-space: pre;
       }
+    }
+  }
+  h3 {
+    max-width: 85%;
+    font-family: "Roboto Mono";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 0.9rem;
+    line-height: 1rem;
+    padding-top: 1rem;
+    padding-bottom: 0rem;
+    @include breakpoint(medium) {
+      font-size: 1rem;
+      line-height: 1.4rem;
+      padding-top: 1.5rem;
+      padding-bottom: 1.5rem;
+    }
+  }
+  h1 {
+    padding: 0rem;
+    text-transform: uppercase;
+    font-size: 2.5rem;
+    line-height: 2rem;
+    font-weight: 800;
+    @include breakpoint(medium) {
+      font-size: 3.5rem;
+      line-height: 3rem;
     }
   }
 }
