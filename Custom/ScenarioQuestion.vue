5713cd371b0e7315a87a682fb2b47c1ef36c20bc
Radames Ajna <radamajna@gmail.com>
2020-11-24 22:09:47 -0800

    scenario overlay

--- Custom/ScenarioQuestion.vue
+++ Custom/ScenarioQuestion.vue
@@ -1,13 +1,12 @@
 <template>
   <swiper-slide class="relative">
-    <div
-      class="absolute w-100 h-100"
-      :style="{
-        backgroundImage: `url(${scenario.image}.gif)`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-      }"
-    ></div>
+    <div class="absolute w-100 h-100 z--1 o-30">
+      <video class="db w-100" autoplay loop muted playsinlines>
+        <source :src="`${scenario.image}.webm`" />
+        <source :src="`${scenario.image}.mp4`" />
+        <img class="db w-100" :src="`${scenario.image}.gif`" />
+      </video>
+    </div>
     <article class="measure lh-copy ph3 relative">
       <div class="bg-green br4 ph3 pv1" v-html="scenario.prompt.text"></div>
       <div class="flex justify-center mt3">
@@ -17,6 +16,18 @@
         <a href="" class="no-underline black bg-green br4 db pa2 grow mh2 tc">
           <span>{{ scenario.option.b.content }}</span>
         </a>
+      </div>
+    </article>
+    <article class="lh-copy absolute left-0 top-3 green">
+      <div class="measure ph3 pv1 bg-black shadow br4">
+        <div v-html="scenario.response.text"></div>
+        <div>{{ scenario.option.a.content }}</div>
+        <div>▓▓▓▓▓▓▓▓▓▓▓▓▓▓ 3.9%</div>
+        <div>{{ scenario.option.b.content }}</div>
+        <div>▓▓▓▓▓▓ 1.9%</div>
+      </div>
+      <div class="ph3">
+        <p>NEXT SCENARIO ></p>
       </div>
     </article>
   </swiper-slide>
@@ -43,4 +54,10 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@import '~@/assets/css/variables';
+
+.shadow {
+  box-shadow: 5px 5px 5px $green;
+}
+</style>
