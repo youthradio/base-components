c3d1a536e95f3b09ce9167e9e55638cd51b4882c
shagia <usenet@shagia.me>
2021-04-08 13:57:20 -0700

    Hide overlay

--- Header/FeatureHeader.vue
+++ Header/FeatureHeader.vue
@@ -78,7 +78,7 @@ export default {
 }
 .full-overlay {
 }
-.base-overlay::after {
+/*.base-overlay::after {
   content: '';
   position: absolute;
   top: 0;
@@ -87,7 +87,7 @@ export default {
   width: 45%;
   background-color: rgba(lighten($feature-header-color, 20%), 0.8);
   z-index: 0;
-}
+}*/
 
 .base-text {
   h2 {
